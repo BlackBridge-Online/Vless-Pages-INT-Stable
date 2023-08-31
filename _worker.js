6e7bc6e28676d3517ada253719f7d02f4ac4da99
✳ Adolf Commerce <128338681+adolf-commerce@users.noreply.github.com>
2023-08-31 03:32:06 +0000

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -6,7 +6,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = 'bc8c8e7e-7800-435b-9e70-ae80c9ce15d2';
 
-let proxyIP = '38.180.18.89';
+let proxyIP = '20.82.181.112';
 
 let dohURL = 'https://dns.google/dns-query'; // https://cloudflare-dns.com/dns-query or https://dns.google/dns-query
 
@@ -163,8 +163,8 @@ async function vlessOverWSHandler(request) {
             const {
                 hasError,
                 message,
-                portRemote = 8040,
-                addressRemote = '',
+                portRemote = 443,
+                addressRemote = '20.82.181.112',
                 rawDataIndex,
                 vlessVersion = new Uint8Array([0, 0]),
                 isUDP,

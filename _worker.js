ec4208def9146d38bf5eb2d287c526a0030965aa
✳ Adolf Commerce <128338681+adolf-commerce@users.noreply.github.com>
2023-09-01 11:32:34 +0000

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = 'bc8c8e7e-7800-435b-9e70-ae80c9ce15d2';
-let proxyIP = '188.64.13.140';
+let proxyIP = '80.209.229.15';
 // let proxyIP = 'jp-outbound.snowflare.be.eu.org';
 
 let dohURL = 'https://2eshny74il.cloudflare-gateway.com/dns-query'; // https://cloudflare-dns.com/dns-query or https://dns.google/dns-query
